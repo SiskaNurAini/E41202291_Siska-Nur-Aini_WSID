a1785dd32acd22af2380e040041d18f607d82c59
SiskaNurAini <siskana1501@gmail.com>
2021-03-16 21:24:01 +0700

    first commit

---
+++ KesalahanVariable/src/kesalahanvariable/KesalahanVariable.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kesalahanvariable;
+
+/**
+ *
+ * @author SISKA NUR AINI
+ */
+public class KesalahanVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //DEKLARASI VARIABLE YANG BENAR
+        Boolean gameOver = false;
+        int student = 50, classes =3;
+        double sales_tax;
+        short number1;
+        
+        //DEKLARASI VARIABLE YANG SALAH
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+        
+    }
+    
+}
